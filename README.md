3681e4a4101ce653f0a612038d8b53c9d1d8f6d2
Sewell Stephens <sewellstephens104@gmail.com>
2024-10-09 09:50:45 -0500

    added debounce code

---
+++ README.md
@@ -0,0 +1,21 @@
+## Any Debounce
+
+A simple debounce package made just for react.
+
+Only dependency is react.
+
+### Installation
+
+```bash
+npm i any-debounce
+```
+
+### Usage
+
+```javascript
+import { useDebounce } from "any-debounce";
+
+const handleOnChange = useDebounce((value) => {
+    
+}, 1000);
+```
---
+++ index.js
@@ -0,0 +1,14 @@
+
+const debounce = (callback, wait) => {
+    let timeoutId = null;
+    return (...args) => {
+      window.clearTimeout(timeoutId);
+      timeoutId = window.setTimeout(() => {
+        callback.apply(null, args);
+      }, wait);
+    };
+}
+
+export const useDebounce = (callback, wait) => {
+    return debounce(callback, wait);
+}
